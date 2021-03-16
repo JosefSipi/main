8cde9180fe92dde502a7529c2f61d0eef6aa916d
Joseph Sipiorski <josephsipiorski@gmail.com>
2021-03-16 07:44:09 -0500

    attempt at Error Handling Funtime

---
+++ W4D2/testing.rb
@@ -0,0 +1,32 @@
+# PHASE 2
+def convert_to_int(str)
+  Integer(str)
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  else 
+    raise StandardError 
+  end 
+end
+
+def feed_me_a_fruit
+  begin
+    puts "Hello, I am a friendly monster. :)"
+
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue 
+    puts "probably wasn't a fruit or something, so now you need to give me coffee and then fruit"
+    FRUITS << "coffee"
+   
+    retry
+  end
+end 
+
+p feed_me_a_fruit
