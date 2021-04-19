9c5fd1602f965585a1dba059c5ad298cb583a991
Joseph <Josephsipiorski@gmail.com>
2021-04-18 20:48:03 -0500

    completed phase 3

--- W9D1/canvas_practice/practice.js
+++ W9D1/canvas_practice/practice.js
@@ -2,5 +2,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const canvas = document.getElementById('mycanvas');
 
-    
+    const ctx = canvas.getContext('2d');
+
+    // ctx.fillRect(0, 0, 500, 500);
+    ctx.beginPath();
+    ctx.arc(250, 250, 200, 0, 2 * Math.PI);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+
 });
